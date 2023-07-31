29600aecfe78f40ad723f5e262ee24a32c94ef02
Maria <mayigd0502@gmail.com>
2023-07-31 09:45:05 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useMemo, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [q, setQ] = useState("");
+  const [things, setThings] = useState([
+    {
+      nombre: "Olla a presion",
+    },
+    {
+      nombre: "Cuchara de palo",
+    },
+    {
+      nombre: "Cuchara de sopa",
+    },
+    {
+      nombre: "colador",
+    },
+    {
+      nombre: "vaso de vidrio",
+    },
+  ]);
+
+  const filteredThings = useMemo(
+    () => things.filter((t) => t.nombre.includes(q)),
+    [q]
+  );
+
+  return (
+    <div>
+      <input type="text" value={q} onChange={(e) => setQ(e.target.value)} />
+      <ul>
+        {filteredThings.map((t) => (
+          <li>{t.nombre}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
